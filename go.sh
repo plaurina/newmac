4edf1a74bd29f5ffb930e597c215b43c1e90a65f
PLaurina <PLaurina@gmail.com>
2021-04-11 17:35:02 -0400

    updated go.sh for brew dropping cask keyword

--- go.sh
+++ go.sh
@@ -14,98 +14,98 @@ xcode-select --install
 echo "###############################################################################"
 echo "# Homebrew + Cask                                                             #"
 echo "###############################################################################"
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
 echo "###############################################################################"
 echo "# Common                                                                      #"
 echo "###############################################################################"
-brew cask install google-chrome
-brew cask install vlc
-brew cask install openoffice
+#brew install --cask google-chrome
+brew install --cask vlc
+brew install --cask openoffice
 
 echo "###############################################################################"
 echo "# Sysadmin                                                                    #"
 echo "###############################################################################"
-brew cask install iterm2
-brew cask install alfred
+brew install --cask iterm2
+brew install --cask alfred
 # softwareupdate --install 'RemoteDesktopClient-3.8.2 v1.1'
-brew cask install atext
-brew cask install spectacle
-brew cask install google-backup-and-sync 
+brew install --cask atext
+brew install --cask spectacle
+#brew install --cask google-backup-and-sync 
 
-brew cask install clipy
-brew cask install coconutbattery
-brew cask install mysql
-brew cask install keka
-#brew cask install flux
+brew install --cask clipy
+brew install --cask coconutbattery
+brew install --cask mysql
+#brew install --cask keka
+#brew install --cask flux
 
 echo "###############################################################################"
 echo "# Development                                                                 #"
 echo "###############################################################################"
-#brew cask install sublime-text3
-#brew cask install virtualbox
-#brew cask install vagrant
-#brew cask install googleappengine
-#brew install mcrypt #required for laravel
-#brew install php56-mcrypt #required for laravel
+#brew install --cask sublime-text3
+brew install --cask virtualbox
+#brew install --cask vagrant
+#brew install --cask googleappengine
+#brew install mcrypt #required  laravel
+#brew install php56-mcrypt #required  laravel
 #vagrant plugin install vagrant-bindfs
 #vagrant plugin install vagrant-hostsupdater
-#brew cask install sourcetree
-#brew cask install sequel-pro
+#brew install --cask sourcetree
+#brew install --cask sequel-pro
 #brew install homebrew/php/composer
-brew install android-platform-tools
-#brew install ansible
+#brew install android-platm-tools
+brew install ansible
 brew install node
 brew install nvm
 npm install -g gulp
 npm install -g bower
-brew cask install docker
-brew cask install kitematic
-brew cask install postman
+brew install --cask docker
+brew install --cask kitematic
+brew install --cask postman
 
-brew cask install notational-velocity
-#brew cask install textwrangler
-brew cask install slack
-#brew cask install screenhero
+brew install --cask notational-velocity
+brew install --cask textwrangler
+#brew install --cask slack
+#brew install --cask screenhero
 brew install git
-#brew cask install github-desktop
+brew install --cask github-desktop
 brew install htop
 
-brew install java11
-brew install java6
+#brew install java11
+#brew install java6
 brew install jenv
 brew install maven
 brew install gradle
 
-#brew cask install yourkit-java-profiler
-brew cask install dbvisualizer
+#brew install --cask yourkit-java-profiler
+brew install --cask dbvisualizer
 brew install jce-unlimited-strength-policy
-brew cask install visualvm
-brew cask install intellij-idea
+brew install --cask visualvm
+brew install --cask intellij-idea
 
 
 echo "###############################################################################"
 echo "# Design                                                                      #"
 echo "###############################################################################"
-#brew cask install adobe-photoshop-cc
-brew cask install gimp
-#brew cask install sigil
+#brew install --cask adobe-photoshop-cc
+#brew install --cask gimp
+#brew install --cask sigil
 
 echo "###############################################################################"
 echo "# Security                                                                    #"
 echo "###############################################################################"
-#brew cask install private-internet-access  not working
-#brew cask install lastpass use browser extension not working
+#brew install --cask private-internet-access  not working
+#brew install --cask lastpass use browser extension
 #brew tap trinitronx/homebrew-truecrypt using integrated mac encryption
 echo "Administrator password required to enable FileVault"
-sudo fdesetup enable
+#sudo fdesetup enable
 
 #echo "###############################################################################"
 #echo "# Sublime Text                                                                #"
 #echo "###############################################################################"
-#https://www.sublimetext.com/forum/viewtopic.php?f=6&t=21024
+#https://www.sublimetext.com/um/viewtopic.php?f=6&t=21024
 #http://sublime-text-unofficial-documentation.readthedocs.org/en/latest/reference/commands.html
 # /opt/homebrew-cask/Caskroom/sublime-text3/Build\ 3083/Sublime\ Text.app/Contents/SharedSupport/bin/subl --command "show_panel {\"panel\": \"console\"}"
 # /opt/homebrew-cask/Caskroom/sublime-text3/Build\ 3083/Sublime\ Text.app/Contents/SharedSupport/bin/subl --command "insert_snippet {\"contents\" : \"import urllib.request,os,hashlib; h = '2915d1851351e5ee549c20394736b442' + '8bc59f460fa1548d1514676163dafc88'; pf = 'Package Control.sublime-package'; ipp = sublime.installed_packages_path(); urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) ); by = urllib.request.urlopen( 'http://packagecontrol.io/' + pf.replace(' ', '%20')).read(); dh = hashlib.sha256(by).hexdigest(); print('Error validating download (got %s instead of %s), please try manual install' % (dh, h)) if dh != h else open(os.path.join( ipp, pf), 'wb' ).write(by)\"}"
@@ -122,23 +122,23 @@ echo "# General UI/UX                                                           
 echo "###############################################################################"
 
 # Set computer name (as done via System Preferences → Sharing)
-sudo scutil --set ComputerName "LM13P"
-sudo scutil --set HostName "LM13P"
-sudo scutil --set LocalHostName LM13P"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "LM13P"
+#sudo scutil --set ComputerName "LM13P"
+#sudo scutil --set HostName "LM13P"
+#sudo scutil --set LocalHostName LM13P"
+#sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "LM13P"
 
 # Expand save panel by default
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateSaveMode2 -bool true
 
 # Expand print panel by default
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStatePrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStatePrint2 -bool true
 
 # Disable the crash reporter
 defaults write com.apple.CrashReporter DialogType -string "none"
 
-# Check for software updates daily, not just once per week
+# Check  software updates daily, not just once per week
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
 # Disable Notification Center and remove the menu bar icon
@@ -149,7 +149,7 @@ echo "##########################################################################
 echo "# Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #"
 echo "###############################################################################"
 
-# Trackpad: enable tap to click for this user and for the login screen
+# Trackpad: enable tap to click  this user and  the login screen
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults write com.apple.AppleMultitouchTrackpad Clicking -int 1
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
@@ -165,7 +165,7 @@ echo "##########################################################################
 # Save screenshots to the desktop
 defaults write com.apple.screencapture location -string "${HOME}/Desktop"
 
-# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+# Save screenshots in PNG mat (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
 
 # Disable shadow in screenshots
@@ -175,8 +175,8 @@ echo "##########################################################################
 echo "# Finder                                                                      #"
 echo "###############################################################################"
 
-# Set the default `PfDe` location for new Finder windows
-# For other paths, use `PfLo` and `file:///full/path/here/`
+# Set the default `PfDe` location  new Finder windows
+#  other paths, use `PfLo` and `file:///full/path/here/`
 defaults write com.apple.finder NewWindowTarget -string "PfLo"
 defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
 
@@ -199,7 +199,7 @@ defaults write com.apple.finder ShowRecentTags -bool false
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
 # Use column view in all Finder windows by default
-# Four-letter codes for the view modes: `icnv` = icon, `clmv` = column, `Flwv` = coverflow, `Nlsv` = list
+# Four-letter codes  the view modes: `icnv` = icon, `clmv` = column, `Flwv` = coverflow, `Nlsv` = list
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
 # Show items in finder sidebar
@@ -237,7 +237,7 @@ echo "# Time Machine                                                            
 echo "###############################################################################"
 
 # Prevent Time Machine from prompting to use new hard drives as backup volume
-defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+defaults write com.apple.TimeMachine DoNotOfferNewDisksBackup -bool true
 
 # Disable local Time Machine backups
 hash tmutil &> /dev/null && sudo tmutil disablelocal
